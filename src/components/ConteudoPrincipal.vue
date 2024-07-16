806aeb7c3c5447cda67d74a92f6bca1cfdfadc74
jovemcarlosdev <carlosandre116@outlook.com>
2024-07-16 19:59:06 -0300

    Componentização da lista

--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -1,6 +1,6 @@
 <script lang="ts">
 import SelecionarIngredientes from './SelecionarIngredientes.vue'
-import Tag from './Tag.vue'
+import SuaLista from './SuaLista.vue'
 
 export default {
   data() {
@@ -9,7 +9,7 @@ export default {
     }
   }, components: {
     SelecionarIngredientes,
-    Tag
+    SuaLista
   }
 }
 </script>
@@ -17,18 +17,7 @@ export default {
 <template>
   <main class="conteudo-principal">
     <section>
-      <span class="subtitulo-lg sua-lista-texto">Sua Lista:</span>
-
-      <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
-        <li v-for="ingrediente in ingredientes" :key="ingrediente">
-          <Tag :texto="ingrediente" ativa />
-        </li>
-      </ul>
-
-      <p v-else class="paragrafo lista-vazia">
-        <img src="../assets/images/icones/lista-vazia.svg" alt="Ícone de pesquisa">
-        Sua lista está vazia, selecione ingredientes para começar a buscar receitas!
-      </p>
+      <SuaLista :ingredientes="ingredientes" />
     </section>
 
     <SelecionarIngredientes />
@@ -46,31 +35,6 @@ export default {
   flex-direction: column;
   align-items: center;
   gap: 5rem;
-}
-
-.sua-lista-texto {
-  color: var(--coral, #F0633C);
-  display: block;
-  text-align: center;
-  margin-bottom: 1.5rem;
-}
-
-.ingredientes-sua-lista {
-  display: flex;
-  justify-content: center;
-  gap: 1rem 1.5rem;
-  flex-wrap: wrap;
-}
-
-.lista-vazia {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-wrap: wrap;
-  gap: 0.25rem;
-
-  color: var(--coral, #F0633C);
-  text-align: center;
 }
 
 @media only screen and (max-width: 1300px) {
